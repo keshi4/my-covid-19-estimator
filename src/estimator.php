f35935fdb0308b43224f928dacc28c47cb2f0de7
keshi <chinoskeshi@gmail.com>
2020-04-09 16:53:26 +0100

    challenge Updates

--- src/estimator.php
+++ src/estimator.php
@@ -20,15 +20,15 @@ function covid19ImpactEstimator($data)
     //Estimate the number of infected by x (28)days, note that currentlyInfected doubles every 3 days
     switch ($periodType){
     case 'days':
-            $factor = floor(($timeToElapse * 1) / 3);
+            $factor = intval(($timeToElapse * 1) / 3);
             $dayPeriod = $timeToElapse * 1;
         break;
     case 'weeks':
-        $factor = floor(($timeToElapse * 7) / 3);
+        $factor = intval(($timeToElapse * 7) / 3);
         $dayPeriod = $timeToElapse * 7;
         break;
     case 'months':
-        $factor = floor(($timeToElapse * 30) / 3);
+        $factor = intval(($timeToElapse * 30) / 3);
         $dayPeriod = $timeToElapse * 30;
         break;
     }
@@ -36,19 +36,19 @@ function covid19ImpactEstimator($data)
     //Output Estimate for impact
     $impact_currentlyInfected = $reportedCases * 10;
     $impact_infectionsByRequestedTime = $impact_currentlyInfected * pow(2, $factor);
-    $impact_severeCasesByRequestedTime = floor($impact_infectionsByRequestedTime * (15/100));
-    $impact_hospitalBedsByRequestedTime = floor(($totalHospitalBeds * (35/100)) - $impact_severeCasesByRequestedTime);
-    $impact_casesForICUByRequestedTime = floor($impact_infectionsByRequestedTime * (5/100));
-    $impact_casesForVentilatorsByRequestedTime = floor($impact_infectionsByRequestedTime * (2/100));
+    $impact_severeCasesByRequestedTime = $impact_infectionsByRequestedTime * (15/100);
+    $impact_hospitalBedsByRequestedTime = ($totalHospitalBeds * (35/100)) - $impact_severeCasesByRequestedTime;
+    $impact_casesForICUByRequestedTime = $impact_infectionsByRequestedTime * (5/100);
+    $impact_casesForVentilatorsByRequestedTime = $impact_infectionsByRequestedTime * (2/100);
     $impact_dollarsInFlight = round($impact_infectionsByRequestedTime * $avgDailyIncomePopulation * $avgDailyIncomeInUSD * $dayPeriod, 2);
 
     //Output Estimate for severeImpact
     $severeImpact_currentlyInfected = $reportedCases * 50;
     $severeImpact_infectionsByRequestedTime = $severeImpact_currentlyInfected * pow(2, $factor);
-    $severeCasesByRequestedTime = floor($severeImpact_infectionsByRequestedTime * (15/100));
-    $severe_hospitalBedsByRequestedTime = floor(($totalHospitalBeds * (35/100)) - $severeCasesByRequestedTime);
-    $severe_casesForICUByRequestedTime = floor($severeImpact_infectionsByRequestedTime * (5/100));
-    $severe_casesForVentilatorsByRequestedTime = floor($severeImpact_infectionsByRequestedTime * (2/100));
+    $severeCasesByRequestedTime = $severeImpact_infectionsByRequestedTime * (15/100);
+    $severe_hospitalBedsByRequestedTime = ($totalHospitalBeds * (35/100)) - $severeCasesByRequestedTime;
+    $severe_casesForICUByRequestedTime = $severeImpact_infectionsByRequestedTime * (5/100);
+    $severe_casesForVentilatorsByRequestedTime = $severeImpact_infectionsByRequestedTime * (2/100);
     $severe_dollarsInFlight = round($severeImpact_infectionsByRequestedTime * $avgDailyIncomePopulation * $avgDailyIncomeInUSD * $dayPeriod, 2);
     /**
      Return Output
@@ -74,7 +74,7 @@ function covid19ImpactEstimator($data)
     'casesForVentilatorsByRequestedTime' => $severe_casesForVentilatorsByRequestedTime,
     'dollarsInFlight' => $severe_dollarsInFlight
     );
-    $dataOuput = array('data' => $dataDecode, 'impact' => $impactData, 'severeImpact' => $severeImpactData);
+    $dataOuput = array('data' => $dataDecode,'estimate' => array('impact' => $impactData, 'severeImpact' => $severeImpactData));
     //return json_encode($dataOuput);
     return $dataOuput;
 }
@@ -84,15 +84,16 @@ $reportedCases = '{
   "region":{
     "name":"Africa",
     "avgAge":19.7,
-    "avgDailyIncomeInUSD":5,
-    "avgDailyIncomePopulation":0.71
+    "avgDailyIncomeInUSD":4,
+    "avgDailyIncomePopulation":0.87
   },
-  "periodType":"days",
-  "timeToElapse":58,
-  "reportedCases":674,
-  "population":66622705,
-  "totalHospitalBeds":1380614
-}';
+  "periodType":"weeks",
+  "timeToElapse":19,
+  "reportedCases":2884,
+  "population":119675889,
+  "totalHospitalBeds":1653977
+
+  }';
 header("Content-Type: application/json");
 echo covid19ImpactEstimator($reportedCases);
 */
